fb78e75a17da249598cd2d4d47cd8d0f419fd0ca
gustavjohn.nafalt <gustavjohn.nafalt@student.kuleuven.be>
2020-02-07 15:57:57 +0100

    hejdå

--- Gemensam kod/od_clustering.m
+++ Gemensam kod/od_clustering.m
@@ -1,36 +1,22 @@
+tic
 % Add jar file to classpath (ensure it is present in your current dir)
 javaclasspath('postgresql-8.3-603.jdbc3.jar');
 
-% Username and password you chose when installing postgres
-props=java.util.Properties;
-props.setProperty('user', 'tnk106');
-props.setProperty('password', 'positioning_systems');
+query = ['select id_ori,id_dest, ST_x(geom_ori) as x_origin, ST_y(geom_ori) as y_origin, ' ...
+'ST_x(geom_dest) as x_dest, ST_y(geom_dest) as y_dest,ST_length(geom_line) as dis from all_od_pairs'] ;
 
-% Create the database connection (port 5432 is the default)
-driver=org.postgresql.Driver;
-url = 'jdbc:postgresql://localhost:5432/senzoor';
-conn=driver.connect(url, props);
+databasename = 'exjobb';
+username = 'gustav';
+password = 'password123';
+driver = 'org.postgresql.Driver';
+url = 'jdbc:postgresql://localhost:5432/exjobb';
 
-sql_fp = ['SELECT *, extract(EPOCH from time) as time2 FROM tnk106.bt_search_group_2 where '] ;
-    sql_fp = strcat(sql_fp,'',' extract(EPOCH from time) > ' ,'', num2str((curTime - t)), ' ');
+conn = database(databasename,username,password,driver,url);
+data = select(conn,query);
 
-    
-    pause(1)
-    ps=conn.prepareStatement(sql_fp);
-    rs=ps.executeQuery();
-    
-    clear row_id ;
-    clear time;
-    clear  mac ;
-    clear  rss ;
-    clear x_est ;
-    clear y_est ;
-    
-    count=1;
-    while rs.next()
-        row_id(count) = rs.getInt('row_id');
-        time(count) = str2double(rs.getString('time2'));
-        mac(count) = rs.getString('mac');
-        rss(count) = rs.getInt('rss');
-        count=count+1;
-    end
+data = table2array(data);
+data = double(data);
+idx = kmedoids((data),50);
+
+close(conn)
+toc
