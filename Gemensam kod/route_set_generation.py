1a90f168cbd2c6bc130d0d8936dfa22980bba444
gustavjohn.nafalt <gustavjohn.nafalt@student.kuleuven.be>
2020-02-13 08:56:33 +0100

    test

--- Gemensam kod/route_set_generation.py
+++ Gemensam kod/route_set_generation.py
@@ -433,6 +433,7 @@ def main():
         # create_tables(start_list_selected, end_list_selected, removed_lid)
 
         print_selected_pairs(start_list, end_list, removed_lid)
+        
 
 
 if __name__ == "__main__" or __name__ == "__console__":
